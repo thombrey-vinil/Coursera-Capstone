633490305be0b795ad68f733325d8b2ea5f19fae
thombrey-vinil <32880384+thombrey-vinil@users.noreply.github.com>
2019-09-07 14:25:11 +0100

    This is for capstone project

---
+++ capstone.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# # This Jypyter Notebook will be used for the Capstone project. 
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np 
+
+
+# In[2]:
+
+
+# Print 
+print("Hello capstone Project Course")
+
